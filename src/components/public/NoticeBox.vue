cf4d27b51e7092709bca9f2a0b8cd9808226bc49
rhw943359178 <qiant@bsoft.com.cn>
2020-05-25 15:20:54 +0800

    自定义表单验证方式，将警告信息封装成组件

---
+++ src/components/public/NoticeBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="notice">
+    谁水水水水水水水水水水水水水水水水水水水
+    <div class="notice-big"></div>
+    <div class="notice-small"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+  @import '@/assets/css/notice.scss';
+</style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,20 +5,22 @@
       <div class="m-sign-box">
         <Header :type="signType" />
         <el-row class="m-sign-body">
-          <el-form>
-            <el-form-item>
-              <el-tooltip class="m-sign-tooltip" effect="light" content="是的没错" placement="right">
-                <el-input v-model="userInfo.name" placeholder="你的名称" style="width: 300px" prefix-icon="el-icon-user-solid" clearable></el-input>
-              </el-tooltip>
+          <el-form :model="userInfo" ref="ruleForm" :rules="rules">
+            <el-form-item prop="username" style="">
+              <el-input v-model="userInfo.username" placeholder="你的名称" style="width: 300px; position: relative" prefix-icon="el-icon-user-solid" clearable></el-input>
+              <NoticeBox v-if="false"/>
             </el-form-item>
-            <el-form-item>
-              <el-input v-model="userInfo.phone" placeholder="手机号或邮箱" style="width: 300px" prefix-icon="el-icon-mobile-phone" clearable></el-input>
+            <el-form-item prop="phoneMail">
+              <el-input v-model="userInfo.phoneMail" placeholder="手机号或邮箱" style="width: 300px" prefix-icon="el-icon-mobile-phone" clearable></el-input>
+              <NoticeBox/>
             </el-form-item>
-            <el-form-item>
+            <el-form-item prop="pwd">
               <el-input v-model="userInfo.pwd" placeholder="设置密码" style="width: 300px" prefix-icon="el-icon-lock" clearable></el-input>
+              <NoticeBox/>
             </el-form-item>
-            <el-form-item>
+            <el-form-item prop="cpwd">
               <el-input v-model="userInfo.cpwd" placeholder="确认密码" style="width: 300px" prefix-icon="el-icon-lock" clearable></el-input>
+              <NoticeBox/>
             </el-form-item>
           </el-form>
         </el-row>
@@ -43,11 +45,13 @@
 import Background from '../components/public/sign/background.vue'
 import Footer from '../components/public/sign/footer.vue'
 import Header from '../components/public/sign/Header.vue'
+import NoticeBox from '../components/public/NoticeBox.vue'
 export default {
   components: {
     Background,
     Footer,
-    Header
+    Header,
+    NoticeBox
   },
   data () {
     return {
@@ -56,7 +60,14 @@ export default {
         name: ''
       },
       //  判断是注册还是登陆，注册是1，登陆是2
-      signType: 1
+      signType: 1,
+
+      //  注册表格校验规则
+      rules: {
+        username: [
+          { required: true, type: 'string', message: '请输入名称', trigger: 'blur' }
+        ]
+      }
     }
   }
 }
